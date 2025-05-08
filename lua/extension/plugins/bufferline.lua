6b5fba11c4caab89df52e4d0346a3a1907a4950f
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-08 17:00:41 +0800

    添加插件: folke/akinsho/bufferline.nvim

---
+++ lua/extension/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'folke/snacks.nvim'
+    },
+    event = 'VeryLazy',
+    keys = {
+        { 'H', '<cmd>BufferLineCyclePrev<cr>', desc = '跳转到上一个 buffer' },
+        { 'L', '<cmd>BufferLineCycleNext<cr>', desc = '跳转到下一个 buffer' },
+        { '<leader>bp', '<cmd>BufferLinePick<cr>', desc = '给每一个 buffer 打上跳转标记' },
+        { '<leader>bd', '<cmd>lua Snacks.bufdelete.delete()<cr>', desc = '关闭当前 buffer' },
+        { '<leader>bo', '<cmd>lua Snacks.bufdelete.other()<cr>', desc = '关闭当前 buffer' }
+    },
+    opts = {
+        options = {
+            indicator = { style = 'underline' },
+            offsets = {
+                {
+                    filetype = 'neo-tree',
+                    text = 'File Explorer',
+                    text_align = 'center',
+                    separator = true
+                }
+            }
+        }
+    }
+}
