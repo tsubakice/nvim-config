ccd6919bf33e99fc1086fa0dfcf18e005001d6b4
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-08 15:21:21 +0800

    添加扩展: lazy.nvim, 一个非常好用的插件管理器

--- lua/extension/init.lua
+++ lua/extension/init.lua
@@ -1,1 +1,2 @@
 require('extension.neovide') -- 加载 neovide 配置
+require('extension.lazy') -- 加载插件管理器 lazy.nvim
---
+++ lua/extension/lazy.lua
@@ -0,0 +1,35 @@
+-- 快速安装并启动 lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'git@github.com:folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy.nvim 相关配置
+require('lazy').setup({
+    git = {
+        log = { '-8' }, -- 插件仓库最近 8 条提交
+        timeout = 180,
+        url_format = 'git@github.com:%s.git',
+        filter = true
+    },
+    change_detection = { notify = false },
+    install = { missing = true }, -- 自动安装缺失插件
+    ui = {
+        wrap = true,
+        border = 'rounded', -- lazy console 边框样式, 可选值请参阅 nvim_open_win()
+        backdrop = 100,
+        title = 'lazy console',
+        title_pos = 'left'
+    }
+})
