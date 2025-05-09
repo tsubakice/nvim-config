4727d827dbb2a64f52ccbee7306c2473f78da221
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-09 15:18:03 +0800

    添加插件: folke/snacks.nvim

---
+++ lua/extension/plugins/snacks.lua
@@ -0,0 +1,38 @@
+return {
+    'folke/snacks.nvim',
+    event = 'VeryLazy',
+    opts = {
+        indent = { enabled = true },
+        quickfile = { enabled = true },
+        bufdelete = { enabled = true },
+        words = {
+            enabled = true,
+            debounce = 100
+        },
+        statuscolumn = {
+            enabled = true,
+            folds = { open = true }
+        },
+        dashboard = {
+            enabled = true,
+            preset = {
+                header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+                keys = {
+                    { icon = ' ', key = '<leader>ff', desc = 'Find File', action = function () require('telescope.builtin').find_files() end },
+                    { icon = ' ', key = '<leader>fg', desc = 'Find Text', action = function () require('telescope.builtin').live_grep() end },
+                    { icon = ' ', key = '<leader>?', desc = 'History Files', action = function () require('telescope.builtin').oldfiles() end },
+                    { icon = ' ', key = '<leader>M', desc = 'Open Mason', action = '<cmd>Mason<cr>' },
+                    { icon = '󰒲 ', key = '<leader>L', desc = 'Open Lazy', action = '<cmd>Lazy<cr>' },
+                    { icon = ' ', key = 'n', desc = 'New File', action = '<cmd>ene | startinsert<cr>' },
+                    { icon = ' ', key = 'q', desc = 'Quit', action = '<cmd>qa<cr>' }
+                }
+            }
+        }
+    }
+}
