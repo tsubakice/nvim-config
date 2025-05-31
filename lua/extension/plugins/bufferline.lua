7bf72d859c491945cda3bd94311307e8cfed20fe
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-31 16:43:13 +0800

    添加插件 akinsho/bufferline.nvim

---
+++ lua/extension/plugins/bufferline.lua
@@ -0,0 +1,32 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = {
+        'folke/tokyonight.nvim',
+        'nvim-tree/nvim-web-devicons'
+    },
+    lazy = false,
+    keys = {
+        { 'H', '<cmd>BufferLineCyclePrev<cr>', desc = '跳转到左侧 buffer' },
+        { 'L', '<cmd>BufferLineCycleNext<cr>', desc = '跳转到右侧 buffer' },
+        { '<leader>bp', '<cmd>BufferLinePick<cr>', desc = '给 buffer 打上跳转标记' },
+        { '<leader>bl', '<cmd>BufferLineCloseLeft<cr>', desc = '关闭左侧 buffer' },
+        { '<leader>br', '<cmd>BufferLineCloseRight<cr>', desc = '关闭右侧 buffer' },
+        { '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', desc = '关闭其他 buffer' }
+    },
+    opts = {
+        options = {
+            indicator = { style = 'underline' },
+            separator_style = 'slope',
+            offsets = {
+                {
+                    filetype = 'neo-tree',
+                    text = 'Resources Manager',
+                    text_align = 'center',
+                    separator = true
+                }
+            },
+            diagnostics = 'nvim_lsp'
+        }
+    }
+}
