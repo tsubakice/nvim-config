bd2766b541eac15f6af21bb4aeb6018ff5b06ede
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-31 16:51:45 +0800

    添加插件 folke/snacks.nvim

---
+++ lua/extension/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+    'folke/snacks.nvim',
+    dependencies = { -- 防止 bufferline 和 lualine 在 snacks_dashboard 中显示异常
+        'akinsho/bufferline.nvim',
+        'nvim-lualine/lualine.nvim'
+    },
+    opts = {
+        bufdelete = { enabled = true }
+    }
+}
