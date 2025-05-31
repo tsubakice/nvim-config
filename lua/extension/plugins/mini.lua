dbebcae3df86c575bf896e0f05b8bfd30429f940
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-31 17:10:19 +0800

    添加插件 echasnovski/mini.*

---
+++ lua/extension/plugins/mini.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        opts = {
+            modes = {
+                insert = true,
+                command = true,
+                terminal = false
+            }
+        }
+    },
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        keys = {
+            { 'sa', 'sa', desc = '添加环绕符号' },
+            { 'sd', 'sd', desc = '删除环绕符号' },
+            { 'sr', 'sr', desc = '替换环绕符号' }
+        },
+        opts = {
+            mappings = { -- 仅启用添加、删除和替换环绕符号
+                add = 'sa',
+                delete = 'sd',
+                replace = 'sr',
+                find = '',
+                find_left = '',
+                highlight = '',
+                update_n_lines = '',
+                suffix_last = '',
+                suffix_next = ''
+            }
+        }
+    }
+}
